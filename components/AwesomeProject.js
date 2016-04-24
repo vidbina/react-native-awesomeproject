1a78a75b87a5269b4a7f3dcf0e91d3b416cdd0ea
David Asabina <david@supr.nu>
2016-04-24 12:39:39 -0400

    Refactored AwesomeProject component out

---
+++ components/AwesomeProject.js
@@ -0,0 +1,79 @@
+import React, {
+  AppRegistry,
+  Component,
+  Image,
+  ListView,
+  Text,
+  View
+} from 'react-native';
+
+import MoviesSource from '../movies';
+import BasicStyle from '../styles/basic';
+
+export default class AwesomeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    console.log("mounted");
+    this.fetchData();
+  }
+
+  fetchData() {
+    console.log("loading...");
+    console.log(MoviesSource);
+    (new MoviesSource()).fetchData((data) => {
+      console.log("here goes");
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(data.movies),
+        loaded: true,
+      });
+    });
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={BasicStyle.listView}
+        />
+    );
+  }
+
+  renderLoadingView() {
+    console.log("This is odd");
+    return (
+      <View style={BasicStyle.container}>
+        <Text>
+          Loading movies
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={BasicStyle.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={BasicStyle.thumbnail} />
+        <View style={BasicStyle.rightContainer}>
+          <Text style={BasicStyle.title}>{movie.title}</Text>
+          <Text style={BasicStyle.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+}
--- index.android.js
+++ index.android.js
@@ -1,86 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
 import React, {
   AppRegistry,
-  Component,
-  Image,
-  ListView,
-  Text,
-  View
 } from 'react-native';
 
-import MoviesSource from './movies';
-import BasicStyle from './styles/basic';
+import AwesomeProject from './components/AwesomeProject';
 
-class AwesomeProject extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
-  }
-
-  componentDidMount() {
-    console.log("mounted");
-    this.fetchData();
-  }
-
-  fetchData() {
-    console.log("loading...");
-    console.log(MoviesSource);
-    (new MoviesSource()).fetchData((data) => {
-      console.log("here goes");
-      this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(data.movies),
-        loaded: true,
-      });
-    });
-  }
-
-  render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
-    return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderMovie}
-        style={BasicStyle.listView}
-        />
-    );
-  }
-
-  renderLoadingView() {
-    console.log("This is odd");
-    return (
-      <View style={BasicStyle.container}>
-        <Text>
-          Loading movies
-        </Text>
-      </View>
-    );
-  }
-
-  renderMovie(movie) {
-    return (
-      <View style={BasicStyle.container}>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={BasicStyle.thumbnail} />
-        <View style={BasicStyle.rightContainer}>
-          <Text style={BasicStyle.title}>{movie.title}</Text>
-          <Text style={BasicStyle.year}>{movie.year}</Text>
-        </View>
-      </View>
-    );
-  }
+class AndroidAwesomeProject extends AwesomeProject {
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', () => AndroidAwesomeProject);
